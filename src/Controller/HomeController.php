01776d9b23451210548f56b89796c476438db108
LujainAljelani <lujain.shellick@gmail.com>
2023-05-24 02:58:52 +0200

    Enlaces en la barra de navegación y nuevos twig para las secciones

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use DateTime;
 
 class HomeController extends AbstractController
-{
+{        
+        public function homepage(): Response
+        {
     
-    public function homepage(): Response
-    {
-        return $this->render('homepage.html.twig');
+            return $this->render('homepage.html.twig');
+        }
+    
+         public function quienesSomos(): Response
+        {
+    
+            return $this->render('quienesSomos.html.twig');
+        }
+    
+        public function actividades(): Response
+        {
+            $now = new DateTime();
+    
+            return $this->render('actividades.html.twig', [
+                'now' => $now,
+            ]
+    
+            );
+        }
     }
-}
